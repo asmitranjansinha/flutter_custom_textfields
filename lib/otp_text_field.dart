fa9e99372a7f2c387e6e82e684631bf920bdb618
asmy8h <sinhaasmit99@gmail.com>
2024-03-01 11:48:44 +0530

    init

---
+++ lib/otp_text_field.dart
@@ -0,0 +1,12 @@
+library flutter_custom_textfields;
+
+import 'package:flutter/material.dart';
+
+class OTPTextField extends StatelessWidget {
+  const OTPTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField();
+  }
+}
